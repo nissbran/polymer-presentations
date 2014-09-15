b7f9bb4b9bfd7b995a57728d3477716eec8a689e
Kristofer Nissbrandt <kristofer.nissbrandt@squeed.com>
2014-09-15 13:42:43 +0200

    Small changes to core ajax demo

--- demos/DemoComponents/UsingCoreAjaxDemo/Models/Customer.cs
+++ demos/DemoComponents/UsingCoreAjaxDemo/Models/Customer.cs
@@ -6,16 +6,10 @@ using System.Web;
 
 namespace UsingCoreAjaxDemo.Models
 {
-    [DataContract]
     public class Customer
     {
-        [DataMember(Name="id")]
         public int Id { get; set; }
-
-        [DataMember(Name = "name")]
         public string Name { get; set; }
-
-        [DataMember(Name = "email")]
         public string Email { get; set; }
     }
 }
